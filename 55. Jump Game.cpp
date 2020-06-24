996bce8a33e575f37934a345d27a8e4e8e5c806d
Zh-al <54863680+Zh-al@users.noreply.github.com>
2020-06-24 10:05:14 +0300

    Create 55. Jump Game.cpp

---
+++ 55. Jump Game.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        for (int i = 0; i < (int)nums.size() - 1; ++i) {
+            if (nums[i] == 0) {
+                if (!canOverJump(nums, i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool canOverJump(const vector<int> &nums, int index) {
+        for (int i = index - 1; i >= 0; --i) {
+            if (nums[i] - (index - i) > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        bool ret = Solution().canJump(nums);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
