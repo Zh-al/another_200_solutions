3da839f6c7ea1519672680453bae0a214a83098e
Zh-al <54863680+Zh-al@users.noreply.github.com>
2020-06-25 19:37:06 +0300

    Create 61. Rotate List.cpp

---
+++ 61. Rotate List.cpp
@@ -0,0 +1,123 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == NULL) {
+            return head;
+        }
+        int size = countLength(head);
+        k = k % size;
+        ListNode* last = getLast(head);
+        // (size - k) % size for if the (size - k) == size
+        for (int i = 0; i < (size - k) % size; ++i) {
+            last->next = head;
+            head = head->next;
+            last = last->next;
+            last->next = nullptr;
+        }
+        return head;
+    }
+    
+    ListNode* getLast(ListNode* head) {
+        if (head == NULL) {
+            return head;
+        }
+        while (head->next != NULL) {
+            head = head->next;
+        }
+        return head;
+    }
+    
+    int countLength(ListNode* head) {
+        int count = 0;
+        while (head != NULL) {
+            ++count;
+            head = head->next;
+        }
+        return count;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+        getline(cin, line);
+        int k = stringToInteger(line);
+        
+        ListNode* ret = Solution().rotateRight(head, k);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
