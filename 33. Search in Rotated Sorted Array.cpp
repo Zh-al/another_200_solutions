e4f7638f02abe986ab0d51d832fbc7abad54b8da
Zh-al <54863680+Zh-al@users.noreply.github.com>
2020-06-26 11:57:34 +0300

    Create 33. Search in Rotated Sorted Array.cpp

---
+++ 33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[left]) { // nums[left] to nums[mid] is sorted
+                if (nums[left] <= target && nums[mid] > target) {
+                    right = mid - 1;
+                } else { 
+                    left = mid + 1;
+                }
+            } else { // nums[mid] to nums[roght] is sorted
+                if (nums[right] >= target && nums[mid] < target) {
+                    left = mid + 1;
+                } else { 
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int target = stringToInteger(line);
+        
+        int ret = Solution().search(nums, target);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
