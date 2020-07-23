dcd54ac929edfe04e0e07294d5cf954138624c91
Zh-al <54863680+Zh-al@users.noreply.github.com>
2020-07-23 10:39:54 +0500

    Create 234. Palindrome Linked List.cpp

---
+++ 234. Palindrome Linked List.cpp
@@ -0,0 +1,101 @@
+//https://leetcode.com/problems/palindrome-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == NULL || head->next == NULL) return true;
+        int length = countLength(head);
+        ListNode* prev = NULL;
+        ListNode* next = head->next;
+        for (int i = 0; i < length / 2; ++i) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        head = prev;
+        if (length & 1) next = next->next;
+        while (head != NULL) {
+            if (head->val != next->val) return false;
+            head = head->next;
+            next = next->next;
+        }
+        return true;
+    }
+    
+    int countLength(ListNode* head) {
+        int count = 0;
+        while (head != NULL) head = head->next, ++count;
+        return count;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+        
+        bool ret = Solution().isPalindrome(head);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
