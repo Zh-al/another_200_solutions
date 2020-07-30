582fe8ed1cc6784896d581b9be9f0aa71c118e1f
Zh-al <54863680+Zh-al@users.noreply.github.com>
2020-07-30 11:05:35 +0500

    Create 541. Reverse String II.cpp

---
+++ 541. Reverse String II.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/reverse-string-ii/
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += 2 * k) {
+            reverse(s.begin() + i, s.begin() + min<int>(i + k, s.size()));
+        }
+        return s;
+    }
+};
