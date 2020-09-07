25803a8c57c99d3b27b5cd8f3cb4f497c0c8fbe3
Zh-al <54863680+Zh-al@users.noreply.github.com>
2020-09-07 12:12:38 +0300

    Create 1450. Number of Students Doing Homework at a Given Time.cpp

---
+++ 1450. Number of Students Doing Homework at a Given Time.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int count = 0;
+        for (int i = 0; i < startTime.size(); ++i) {
+            if (startTime[i] <= queryTime) ++count;
+            if (endTime[i] < queryTime) --count;
+        }
+        return count;
+    }
+};
